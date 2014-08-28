4b803540cbffea43d380f5fd00f5701fa7ef81c2
Antoine Musso <hashar@free.fr>
2014-08-28 23:08:38 +0200

    Cloner: Implement cache-no-hardlinks argument
    The cache directory is prefixing with 'file://' which forces in
    git-clone to always copy files. This is intended for OpenStack usage.
    
    By default, git has a feature to hardlink files if the source
    and destination on the same disk. This saves space and speeds up
    the cloning process.
    
    This exposes that feature to users of zuul-cloner.
    
    Change-Id: Ib8796f9dc5ccfeac4a3c9b3d30629c7db0f84739

--- zuul/cmd/cloner.py
+++ zuul/cmd/cloner.py
@@ -58,6 +58,9 @@ class Cloner(zuul.cmd.ZuulApp):
                             help=('a directory that holds cached copies of '
                                   'repos from which to make an initial clone.'
                                   ))
+        parser.add_argument('--cache-no-hardlinks', dest='cache_no_hardlinks',
+                            help=('force git-clone to never use hardlinks when'
+                                  'fetching from the cache directory.'))
         parser.add_argument('git_base_url',
                             help='reference repo to clone from')
         parser.add_argument('projects', nargs='+',
@@ -147,6 +150,7 @@ class Cloner(zuul.cmd.ZuulApp):
             clone_map_file=self.args.clone_map_file,
             project_branches=project_branches,
             cache_dir=self.args.cache_dir,
+            cache_no_hardlinks=self.args.cache_no_hardlinks
         )
         cloner.execute()
 
