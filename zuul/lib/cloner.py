e16f29f307a1d9fd56b7701ee9233590c87a2ff6
Antoine Musso <hashar@free.fr>
2015-11-24 14:11:05 +0100

    zuul-cloner: recognizes bare repos in cache dir
    A git cache populated with non-bare repositories (ie workspace checked
    out) has a couple issues:
    
    * the working copy checkout is unneeded for caching purposes and
      uselessly consumes disk space.  That makes disk images larger than
      strictly needed.
    
    * checked out files might conflict with repositories names. git bare
      clones are suffixed with '.git' which reduces the risk.
    
    Recognize bare clones before non-bare clones.
    Short circuit the logic when neither is found.
    Rename git_cache to repo_cache to avoid confusion since it now varies.
    
    Cover bare repos suffixed with '.git' are properly recognized by
    inspecing the logging message emitted when the cache is hit.
    
    Change-Id: I3d5177787de5c4d629969cee1f715d692275ad9a

--- zuul/lib/cloner.py
+++ zuul/lib/cloner.py
@@ -78,25 +78,34 @@ class Cloner(object):
     def cloneUpstream(self, project, dest):
         # Check for a cached git repo first
         git_cache = '%s/%s' % (self.cache_dir, project)
+        git_cache_bare = '%s.git' % (git_cache)
         git_upstream = '%s/%s' % (self.git_url, project)
+
         repo_is_cloned = os.path.exists(os.path.join(dest, '.git'))
-        if (self.cache_dir and
-            os.path.exists(git_cache) and
-            not repo_is_cloned):
 
-            if self.cache_no_hardlinks:
-                # file:// tells git not to hard-link across repos
-                git_cache = 'file://%s' % git_cache
+        repo_cache = None
+        if (self.cache_dir and not repo_is_cloned):
+            if os.path.exists(git_cache_bare):
+                repo_cache = git_cache_bare
+            elif os.path.exists(git_cache):
+                repo_cache = git_cache
 
-            self.log.info("Creating repo %s from cache %s",
-                          project, git_cache)
-            new_repo = git.Repo.clone_from(git_cache, dest)
-            self.log.info("Updating origin remote in repo %s to %s",
-                          project, git_upstream)
-            new_repo.remotes.origin.config_writer.set('url', git_upstream)
-        else:
+            if repo_cache:
+                if self.cache_no_hardlinks:
+                    # file:// tells git not to hard-link across repos
+                    repo_cache = 'file://%s' % repo_cache
+
+                self.log.info("Creating repo %s from cache %s",
+                              project, repo_cache)
+                new_repo = git.Repo.clone_from(repo_cache, dest)
+                self.log.info("Updating origin remote in repo %s to %s",
+                              project, git_upstream)
+                new_repo.remotes.origin.config_writer.set('url', git_upstream)
+
+        if not repo_cache:
             self.log.info("Creating repo %s from upstream %s",
                           project, git_upstream)
+
         repo = Repo(
             remote=git_upstream,
             local=dest,
