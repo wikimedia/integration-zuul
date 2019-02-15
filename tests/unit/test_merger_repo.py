7fda65d3761339cdb29e03b360b5adf7a9d389de
Paladox <thomasmulhall410@yahoo.com>
2019-02-15 15:18:46 +0000

    [WIP] Upgrade to zuul version 3
    Change-Id: Iea80978f8d4dd73fc68a474cf248e34693ba1e9f

---
+++ tests/unit/test_merger_repo.py
@@ -0,0 +1,265 @@
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+# Copyright 2014 Wikimedia Foundation Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import os
+
+import git
+import testtools
+
+from zuul.merger.merger import Repo
+from tests.base import ZuulTestCase, FIXTURE_DIR, simple_layout
+
+
+class TestMergerRepo(ZuulTestCase):
+
+    log = logging.getLogger("zuul.test.merger.repo")
+    tenant_config_file = 'config/single-tenant/main.yaml'
+    workspace_root = None
+
+    def setUp(self):
+        super(TestMergerRepo, self).setUp()
+        self.workspace_root = os.path.join(self.test_root, 'workspace')
+
+    def test_ensure_cloned(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+
+        # Forge a repo having a submodule
+        parent_repo = git.Repo(parent_path)
+        parent_repo.git.submodule('add', os.path.join(
+            self.upstream_root, 'org/project2'), 'subdir')
+        parent_repo.index.commit('Adding project2 as a submodule in subdir')
+        # git 1.7.8 changed .git from being a directory to a file pointing
+        # to the parent repository /.git/modules/*
+        self.assertTrue(os.path.exists(
+            os.path.join(parent_path, 'subdir', '.git')),
+            msg='.git file in submodule should be a file')
+
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+        self.assertTrue(
+            os.path.isdir(os.path.join(self.workspace_root, 'subdir')),
+            msg='Cloned repository has a submodule placeholder directory')
+        self.assertFalse(os.path.exists(
+            os.path.join(self.workspace_root, 'subdir', '.git')),
+            msg='Submodule is not initialized')
+
+        sub_repo = Repo(
+            os.path.join(self.upstream_root, 'org/project2'),
+            os.path.join(self.workspace_root, 'subdir'),
+            'none@example.org', 'User Name', '0', '0')
+        self.assertTrue(os.path.exists(
+            os.path.join(self.workspace_root, 'subdir', '.git')),
+            msg='Cloned over the submodule placeholder')
+
+        self.assertEqual(
+            os.path.join(self.upstream_root, 'org/project1'),
+            work_repo.createRepoObject().remotes[0].url,
+            message="Parent clone still point to upstream project1")
+
+        self.assertEqual(
+            os.path.join(self.upstream_root, 'org/project2'),
+            sub_repo.createRepoObject().remotes[0].url,
+            message="Sub repository points to upstream project2")
+
+    def test_set_refs(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        remote_sha = self.create_commit('org/project1')
+        self.create_branch('org/project1', 'foobar')
+
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+        repo = git.Repo(self.workspace_root)
+        new_sha = repo.heads.foobar.commit.hexsha
+
+        work_repo.setRefs({'refs/heads/master': new_sha}, True)
+        self.assertEqual(work_repo.getBranchHead('master').hexsha, new_sha)
+        self.assertIn('master', repo.remotes.origin.refs)
+
+        work_repo.setRefs({'refs/heads/master': remote_sha})
+        self.assertEqual(work_repo.getBranchHead('master').hexsha, remote_sha)
+        self.assertNotIn('master', repo.remotes.origin.refs)
+
+    def test_set_remote_ref(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        commit_sha = self.create_commit('org/project1')
+        self.create_commit('org/project1')
+
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+        work_repo.setRemoteRef('master', commit_sha)
+        work_repo.setRemoteRef('invalid', commit_sha)
+
+        repo = git.Repo(self.workspace_root)
+        self.assertEqual(repo.remotes.origin.refs.master.commit.hexsha,
+                         commit_sha)
+        self.assertNotIn('invalid', repo.remotes.origin.refs)
+
+    def test_clone_timeout(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        self.patch(git.Git, 'GIT_PYTHON_GIT_EXECUTABLE',
+                   os.path.join(FIXTURE_DIR, 'fake_git.sh'))
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0',
+                         git_timeout=0.001, retry_attempts=1)
+        # TODO: have the merger and repo classes catch fewer
+        # exceptions, including this one on initialization.  For the
+        # test, we try cloning again.
+        with testtools.ExpectedException(git.exc.GitCommandError,
+                                         r'.*exit code\(-9\)'):
+            work_repo._ensure_cloned()
+
+    def test_fetch_timeout(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0',
+                         retry_attempts=1)
+        work_repo.git_timeout = 0.001
+        self.patch(git.Git, 'GIT_PYTHON_GIT_EXECUTABLE',
+                   os.path.join(FIXTURE_DIR, 'fake_git.sh'))
+        with testtools.ExpectedException(git.exc.GitCommandError,
+                                         r'.*exit code\(-9\)'):
+            work_repo.update()
+
+    def test_fetch_retry(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0',
+                         retry_interval=1)
+        self.patch(git.Git, 'GIT_PYTHON_GIT_EXECUTABLE',
+                   os.path.join(FIXTURE_DIR, 'git_fetch_error.sh'))
+        work_repo.update()
+        # This is created on the first fetch
+        self.assertTrue(os.path.exists(os.path.join(
+            self.workspace_root, 'stamp1')))
+        # This is created on the second fetch
+        self.assertTrue(os.path.exists(os.path.join(
+            self.workspace_root, 'stamp2')))
+
+    def test_deleted_local_ref(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        self.create_branch('org/project1', 'foobar')
+
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+
+        # Delete local ref on the cached repo. This leaves us with a remote
+        # ref but no local ref anymore.
+        gitrepo = git.Repo(work_repo.local_path)
+        gitrepo.delete_head('foobar', force=True)
+
+        # Delete the branch upstream.
+        self.delete_branch('org/project1', 'foobar')
+
+        # And now reset the repo again. This should not crash
+        work_repo.reset()
+
+    def test_broken_cache(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+        self.waitUntilSettled()
+
+        # Break the work repo
+        path = work_repo.local_path
+        os.remove(os.path.join(path, '.git/HEAD'))
+
+        # And now reset the repo again. This should not crash
+        work_repo.reset()
+
+        # Now open a cache repo and break it in a way that git.Repo is happy
+        # at first but git won't be.
+        merger = self.executor_server.merger
+        cache_repo = merger.getRepo('gerrit', 'org/project')
+        with open(os.path.join(cache_repo.local_path, '.git/HEAD'), 'w'):
+            pass
+        cache_repo.update()
+
+    def test_broken_gitmodules(self):
+        parent_path = os.path.join(self.upstream_root, 'org/project1')
+        work_repo = Repo(parent_path, self.workspace_root,
+                         'none@example.org', 'User Name', '0', '0')
+        self.waitUntilSettled()
+
+        # Break the gitmodules
+        path = work_repo.local_path
+        with open(os.path.join(path, '.gitmodules'), 'w') as f:
+            f.write('[submodule "libfoo"]\n'
+                    'path = include/foo\n'
+                    '---\n'
+                    'url = git://example.com/git/lib.git')
+
+        # And now reset the repo again. This should not crash
+        work_repo.reset()
+
+
+class TestMergerWithAuthUrl(ZuulTestCase):
+    config_file = 'zuul-github-driver.conf'
+
+    git_url_with_auth = True
+
+    @simple_layout('layouts/merging-github.yaml', driver='github')
+    def test_changing_url(self):
+        """
+        This test checks that if getGitUrl returns different urls for the same
+        repo (which happens if an access token is part of the url) then the
+        remote urls are changed in the merger accordingly. This tests directly
+        the merger.
+        """
+
+        merger = self.executor_server.merger
+        repo = merger.getRepo('github', 'org/project')
+        first_url = repo.remote_url
+
+        repo = merger.getRepo('github', 'org/project')
+        second_url = repo.remote_url
+
+        # the urls should differ
+        self.assertNotEqual(first_url, second_url)
+
+    @simple_layout('layouts/merging-github.yaml', driver='github')
+    def test_changing_url_end_to_end(self):
+        """
+        This test checks that if getGitUrl returns different urls for the same
+        repo (which happens if an access token is part of the url) then the
+        remote urls are changed in the merger accordingly. This is an end to
+        end test.
+        """
+
+        A = self.fake_github.openFakePullRequest('org/project', 'master',
+                                                 'PR title')
+        self.fake_github.emitEvent(A.getCommentAddedEvent('merge me'))
+        self.waitUntilSettled()
+        self.assertTrue(A.is_merged)
+
+        # get remote url of org/project in merger
+        repo = self.executor_server.merger.repos.get('github.com/org/project')
+        self.assertIsNotNone(repo)
+        git_repo = git.Repo(repo.local_path)
+        first_url = list(git_repo.remotes[0].urls)[0]
+
+        B = self.fake_github.openFakePullRequest('org/project', 'master',
+                                                 'PR title')
+        self.fake_github.emitEvent(B.getCommentAddedEvent('merge me again'))
+        self.waitUntilSettled()
+        self.assertTrue(B.is_merged)
+
+        repo = self.executor_server.merger.repos.get('github.com/org/project')
+        self.assertIsNotNone(repo)
+        git_repo = git.Repo(repo.local_path)
+        second_url = list(git_repo.remotes[0].urls)[0]
+
+        # the urls should differ
+        self.assertNotEqual(first_url, second_url)
--- zuul/lib/connections.py
+++ zuul/lib/connections.py
@@ -12,55 +12,186 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import logging
 import re
+from collections import OrderedDict
+from urllib.parse import urlparse
 
-import zuul.connection.gerrit
-import zuul.connection.smtp
+import zuul.driver.zuul
+import zuul.driver.gerrit
+import zuul.driver.git
+import zuul.driver.github
+import zuul.driver.smtp
+import zuul.driver.timer
+import zuul.driver.sql
+import zuul.driver.bubblewrap
+import zuul.driver.nullwrap
+import zuul.driver.mqtt
+from zuul.connection import BaseConnection
+from zuul.driver import SourceInterface
 
 
-def configure_connections(config):
-    # Register connections from the config
+class DefaultConnection(BaseConnection):
+    pass
 
-    # TODO(jhesketh): import connection modules dynamically
-    connections = {}
 
-    for section_name in config.sections():
-        con_match = re.match(r'^connection ([\'\"]?)(.*)(\1)$',
-                             section_name, re.I)
-        if not con_match:
-            continue
-        con_name = con_match.group(2)
-        con_config = dict(config.items(section_name))
+class ConnectionRegistry(object):
+    """A registry of connections"""
 
-        if 'driver' not in con_config:
-            raise Exception("No driver specified for connection %s."
-                            % con_name)
+    log = logging.getLogger("zuul.ConnectionRegistry")
 
-        con_driver = con_config['driver']
+    def __init__(self):
+        self.connections = OrderedDict()
+        self.drivers = {}
 
-        # TODO(jhesketh): load the required class automatically
-        if con_driver == 'gerrit':
-            connections[con_name] = \
-                zuul.connection.gerrit.GerritConnection(con_name,
-                                                        con_config)
-        elif con_driver == 'smtp':
-            connections[con_name] = \
-                zuul.connection.smtp.SMTPConnection(con_name, con_config)
-        else:
-            raise Exception("Unknown driver, %s, for connection %s"
-                            % (con_config['driver'], con_name))
+        self.registerDriver(zuul.driver.zuul.ZuulDriver())
+        self.registerDriver(zuul.driver.gerrit.GerritDriver())
+        self.registerDriver(zuul.driver.git.GitDriver())
+        self.registerDriver(zuul.driver.github.GithubDriver())
+        self.registerDriver(zuul.driver.smtp.SMTPDriver())
+        self.registerDriver(zuul.driver.timer.TimerDriver())
+        self.registerDriver(zuul.driver.sql.SQLDriver())
+        self.registerDriver(zuul.driver.bubblewrap.BubblewrapDriver())
+        self.registerDriver(zuul.driver.nullwrap.NullwrapDriver())
+        self.registerDriver(zuul.driver.mqtt.MQTTDriver())
 
-    # If the [gerrit] or [smtp] sections still exist, load them in as a
-    # connection named 'gerrit' or 'smtp' respectfully
+    def registerDriver(self, driver):
+        if driver.name in self.drivers:
+            raise Exception("Driver %s already registered" % driver.name)
+        self.drivers[driver.name] = driver
 
-    if 'gerrit' in config.sections():
-        connections['gerrit'] = \
-            zuul.connection.gerrit.GerritConnection(
-                'gerrit', dict(config.items('gerrit')))
+    def registerScheduler(self, sched, load=True):
+        for driver_name, driver in self.drivers.items():
+            if hasattr(driver, 'registerScheduler'):
+                driver.registerScheduler(sched)
+        for connection_name, connection in self.connections.items():
+            connection.registerScheduler(sched)
+            if load:
+                connection.onLoad()
 
-    if 'smtp' in config.sections():
-        connections['smtp'] = \
-            zuul.connection.smtp.SMTPConnection(
-                'smtp', dict(config.items('smtp')))
+    def reconfigureDrivers(self, tenant):
+        for driver in self.drivers.values():
+            if hasattr(driver, 'reconfigure'):
+                driver.reconfigure(tenant)
 
-    return connections
+    def stop(self):
+        for connection_name, connection in self.connections.items():
+            connection.onStop()
+        for driver in self.drivers.values():
+            driver.stop()
+
+    def configure(self, config, source_only=False, include_drivers=None):
+        # Register connections from the config
+        connections = OrderedDict()
+
+        for section_name in config.sections():
+            con_match = re.match(r'^connection ([\'\"]?)(.*)(\1)$',
+                                 section_name, re.I)
+            if not con_match:
+                continue
+            con_name = con_match.group(2)
+            con_config = dict(config.items(section_name))
+
+            if 'driver' not in con_config:
+                raise Exception("No driver specified for connection %s."
+                                % con_name)
+
+            con_driver = con_config['driver']
+            if con_driver not in self.drivers:
+                raise Exception("Unknown driver, %s, for connection %s"
+                                % (con_config['driver'], con_name))
+
+            driver = self.drivers[con_driver]
+
+            # The merger and the reporter only needs source driver.
+            # This makes sure Reporter like the SQLDriver are only created by
+            # the scheduler process
+            if source_only and not isinstance(driver, SourceInterface):
+                continue
+
+            # Zuul web needs only the SQL driver, accomodate that here:
+            if include_drivers is not None:
+                found = False
+                for driver_class in include_drivers:
+                    if isinstance(driver, driver_class):
+                        found = True
+                        break
+                if not found:
+                    continue
+
+            connection = driver.getConnection(con_name, con_config)
+            connections[con_name] = connection
+
+        # If the [gerrit] or [smtp] sections still exist, load them in as a
+        # connection named 'gerrit' or 'smtp' respectfully
+
+        if 'gerrit' in config.sections():
+            if 'gerrit' in connections:
+                self.log.warning(
+                    "The legacy [gerrit] section will be ignored in favour"
+                    " of the [connection gerrit].")
+            else:
+                driver = self.drivers['gerrit']
+                connections['gerrit'] = \
+                    driver.getConnection(
+                        'gerrit', dict(config.items('gerrit')))
+
+        if 'smtp' in config.sections():
+            if 'smtp' in connections:
+                self.log.warning(
+                    "The legacy [smtp] section will be ignored in favour"
+                    " of the [connection smtp].")
+            else:
+                driver = self.drivers['smtp']
+                connections['smtp'] = \
+                    driver.getConnection(
+                        'smtp', dict(config.items('smtp')))
+
+        # Create default connections for drivers which need no
+        # connection information (e.g., 'timer' or 'zuul').
+        if not source_only:
+            for driver in self.drivers.values():
+                if not hasattr(driver, 'getConnection'):
+                    connections[driver.name] = DefaultConnection(
+                        driver, driver.name, {})
+
+        self.connections = connections
+
+    def getSource(self, connection_name):
+        connection = self.connections[connection_name]
+        return connection.driver.getSource(connection)
+
+    def getSources(self):
+        sources = []
+        for connection in self.connections.values():
+            if hasattr(connection.driver, 'getSource'):
+                sources.append(connection.driver.getSource(connection))
+        return sources
+
+    def getReporter(self, connection_name, pipeline, config=None):
+        connection = self.connections[connection_name]
+        return connection.driver.getReporter(connection, pipeline, config)
+
+    def getTrigger(self, connection_name, config=None):
+        connection = self.connections[connection_name]
+        return connection.driver.getTrigger(connection, config)
+
+    def getSourceByHostname(self, hostname):
+        for connection in self.connections.values():
+            if hasattr(connection, 'canonical_hostname'):
+                if connection.canonical_hostname == hostname:
+                    return self.getSource(connection.connection_name)
+            if hasattr(connection, 'server'):
+                if connection.server == hostname:
+                    return self.getSource(connection.connection_name)
+            if hasattr(connection, 'baseurl'):
+                if urlparse(connection.baseurl).hostname == hostname:
+                    return self.getSource(connection.connection_name)
+        return None
+
+    def getSourceByCanonicalHostname(self, canonical_hostname):
+        for connection in self.connections.values():
+            if hasattr(connection, 'canonical_hostname'):
+                if connection.canonical_hostname == canonical_hostname:
+                    return self.getSource(connection.connection_name)
+        return None
