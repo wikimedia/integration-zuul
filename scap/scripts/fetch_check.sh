2b5409e040be9d868c31c44a292bbfc2e36a81e4
Paladox <thomasmulhall410@yahoo.com>
2019-02-15 15:18:46 +0000

    [WIP] Upgrade to zuul version 3
    Change-Id: Iea80978f8d4dd73fc68a474cf248e34693ba1e9f

--- scap/scripts/fetch_check.sh
+++ scap/scripts/fetch_check.sh
@@ -7,6 +7,6 @@ venv="${deploy_dir}/venv"
 # Install python libs.
 cd $deploy_dir
 mkdir -p $venv
-virtualenv --python python2 --system-site-packages --never-download $venv
-[ -f $deploy_dir/wheels/pip-*.whl ] && $venv/bin/pip install --use-wheel --no-deps $deploy_dir/wheels/pip-*.whl
-$venv/bin/pip install --use-wheel --no-deps $deploy_dir/wheels/*.whl
+virtualenv --python python3 --system-site-packages --never-download $venv
+[ -f $deploy_dir/wheels/pip-*.whl ] && $venv/bin/pip3 install --use-wheel --no-deps $deploy_dir/wheels/pip-*.whl
+$venv/bin/pip3 install --use-wheel --no-deps $deploy_dir/wheels/*.whl
