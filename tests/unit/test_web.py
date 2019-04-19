blurrycontour/zuul-gerrit

web: add triggers information to pipeline list
This change adds trigger name and connection name to the pipeline
list. This can be used by web interface to adjust the display of
project's configuration based on triggers presence.

Change-Id: I9a52ec8ccf08b44c5254f12f3ee1ee6708d66118

tests/unit/test_web.py
@@ -403,10 +403,11 @@ class TestWeb(BaseTestWeb):
         # can we fetch the list of pipelines
         data = self.get_url('api/tenant/tenant-one/pipelines').json()
 
+        gerrit_trigger = {'connection': 'gerrit', 'name': 'gerrit'}
         expected_list = [
-            {'name': 'check'},
-            {'name': 'gate'},
-            {'name': 'post'},
+            {'name': 'check', 'triggers': [gerrit_trigger]},
+            {'name': 'gate', 'triggers': [gerrit_trigger]},
+            {'name': 'post', 'triggers': [gerrit_trigger]},
         ]
         self.assertEqual(expected_list, data)
 

wikimedia/integration-zuul

tests/unit/test_web.py
@@ -480,10 +480,13 @@ class TestWeb(BaseTestWeb):
         # can we fetch the list of pipelines
         data = self.get_url('api/tenant/tenant-one/pipelines').json()
 
+        gerrit_trigger = {'name': 'gerrit', 'driver': 'gerrit'}
+        timer_trigger = {'name': 'timer', 'driver': 'timer'}
         expected_list = [
-            {'name': 'check'},
-            {'name': 'gate'},
-            {'name': 'post'},
+            {'name': 'check', 'triggers': [gerrit_trigger]},
+            {'name': 'gate', 'triggers': [gerrit_trigger]},
+            {'name': 'post', 'triggers': [gerrit_trigger]},
+            {'name': 'periodic', 'triggers': [timer_trigger]},
         ]
         self.assertEqual(expected_list, data)
 