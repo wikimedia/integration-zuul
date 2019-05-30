ba6306bc1f2588f52725b0c5ac78080850473d9e
Clark Boylan <clark.boylan@gmail.com>
2019-05-30 13:15:18 -0700

    Update axios version and yarn.lock
    Axios <= 0.18.0 should apparently not be used any longer. Bump this to
    0.19.0 and regenerate the yarn lock. This does update all the other
    packages too because that is how yarn.lock works.
    
    Note we have to fix the linter errors in the process (and clear out an
    annoying warning).
    
    Change-Id: Iabf5171ce8e0ccfabbebc7784da3299b2fd04693

--- web/src/pages/Stream.jsx
+++ web/src/pages/Stream.jsx
@@ -1,4 +1,4 @@
-/* global clearTimeout, setTimeout, JSON, URLSearchParams */
+/* global JSON, URLSearchParams */
 // Copyright 2018 Red Hat, Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
