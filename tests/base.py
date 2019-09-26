f1cc190e704d56f35310e9454a79339a4efdcae4
Fabien Boucher <fboucher@redhat.com>
2019-09-26 15:46:09 +0200

    Pagure - Support for branch creation/deletion
    Thanks to:
    - https://pagure.io/pagure/pull-request/4625 event branch creation
    - https://pagure.io/pagure/pull-request/4626 oldrev for branch update
    
    This patch:
    - Handles git.branch.creation and git.branch.deletion events.
    - Adds support for old_commit event attribute.
    
    It also removes unexisting stop_commit event atribute handling,
    it is only end_commit.
    
    Change-Id: I10423b654df48436bb0ad11c2043c2d67cac7780

--- tests/base.py
+++ tests/base.py
@@ -1453,7 +1453,8 @@ class FakePagureConnection(pagureconnection.PagureConnection):
             'msg': {
                 'project_fullname': project,
                 'branch': 'master',
-                'stop_commit': headsha,
+                'end_commit': headsha,
+                'old_commit': '1' * 40,
             },
             'msg_id': str(uuid.uuid4()),
             'timestamp': 1427459070,
@@ -1472,6 +1473,20 @@ class FakePagureConnection(pagureconnection.PagureConnection):
             'msg_id': str(uuid.uuid4()),
             'timestamp': 1427459070,
             'topic': 'git.tag.creation',
+        }
+        return (name, data)
+
+    def getGitBranchEvent(self, project, branch, type, rev):
+        name = 'pg_push'
+        data = {
+            'msg': {
+                'project_fullname': project,
+                'branch': branch,
+                'rev': rev,
+            },
+            'msg_id': str(uuid.uuid4()),
+            'timestamp': 1427459070,
+            'topic': 'git.branch.%s' % type,
         }
         return (name, data)
 
