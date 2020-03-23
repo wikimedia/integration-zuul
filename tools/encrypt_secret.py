17a437dcee80af54a00d37005aac647d663d2e79
Monty Taylor <mordred@inaugust.com>
2020-03-23 13:06:10 -0500

    Strip by default in tools/encrypt_secret
    Trailing whitespace (newlines) in secrets is almost
    never what people want, but it's easy to leave them in
    and then wind up with hard to debug issues. Switch the
    defaut - make a new option "--no-strip" that will disable
    the behavior.
    
    Change-Id: I46947e38807b55e5cc3bacc060f5d41a63b564b8

--- tools/encrypt_secret.py
+++ tools/encrypt_secret.py
@@ -56,8 +56,11 @@ def main():
                         default=None,
                         help="The name of the Zuul tenant.  This may be "
                         "required in a multi-tenant environment.")
-    parser.add_argument('--strip', action='store_true', default=False,
-                        help="Strip whitespace from beginning/end of input.")
+    parser.add_argument('--strip', default=None,
+                        help='Unused, kept for backward compatibility.')
+    parser.add_argument('--no-strip', action='store_true', default=False,
+                        help="Do not strip whitespace from beginning or "
+                        "end of input.")
     parser.add_argument('--infile',
                         default=None,
                         help="A filename whose contents will be encrypted.  "
@@ -119,7 +122,7 @@ def main():
         plaintext = sys.stdin.read()
 
     plaintext = plaintext.encode("utf-8")
-    if args.strip:
+    if not args.no_strip:
         plaintext = plaintext.strip()
 
     pubkey_file = tempfile.NamedTemporaryFile(delete=False)
